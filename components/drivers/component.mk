2fea192353850c508ec045fbc09e35265fca6ff2
Calin <botofancalin@gmail.com>
2019-01-09 03:35:31 +0200

    Working Cmake build system

--- components/drivers/component.mk
+++ components/drivers/component.mk
@@ -3,10 +3,12 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-COMPONENT_SRCDIRS := ./display/ILI9341 \
-                    ./input/touch/XPT2046
+COMPONENT_SRCDIRS := ./param \
+		     ./display/ILI9341 \
+                     ./input/touch/XPT2046
 
 COMPONENT_ADD_INCLUDEDIRS := . \
                     ./include \
+		    ./param/include \
                     ./display/ILI9341/include \
                     ./input/touch/XPT2046/include
